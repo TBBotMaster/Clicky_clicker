c8c60c563e160abb9fac251b0f12c692436d80e8
TBBotMaster <crypthasher@proton.me>
2024-07-02 23:25:46 +0300

    Added click

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
 import "./MainPage.css";
 
 function MainPage() {
-    return <>
-    </>;
-  }
-  
-  export default MainPage;
+  const [counter, setCounter] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  const [boostTimer, setBoostTimer] = useState(0);
+
+  useEffect(() => {}, [boostTimer]);
+
+  const plusOne = () => {
+    setCounter(counter + multiplier);
+  };
+
+  const fireOn = () => {
+    console.log("in");
+    setMultiplier(multiplier * 5);
+    setBoostTimer(boostTimer + 30);
+    console.log("something " + boostTimer);
+    for (let i = 0; i < 30; i++) {
+      console.log("Start");
+      setTimeout("", 1000);
+      setBoostTimer(29 - i);
+      console.log(boostTimer);
+    }
+    setMultiplier(1);
+  };
+
+  return (
+    <div className="container">
+      <div className="score">Score: {counter}</div>
+      <button className="score-button" onClick={plusOne}>
+        Click me!
+      </button>
+      <button className="boost-button" onClick={fireOn}>
+        &#128293;
+      </button>
+      <div className="boost-timer">{boostTimer}</div>
+    </div>
+  );
+}
+
+export default MainPage;
